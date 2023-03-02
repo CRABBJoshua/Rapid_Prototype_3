3cb32e5e03da44eebb55f8b993e6ee0cf85bfa44
CRABBJoshua <115218634+CRABBJoshua@users.noreply.github.com>
2023-03-02 14:33:56 +0000

    Portal Update

--- Assets/Scripts/Custom Scipts/Portal.cs
+++ Assets/Scripts/Custom Scipts/Portal.cs
@@ -6,19 +6,21 @@ public class Portal : MonoBehaviour
 {
     public GameObject CurrentPortal;
     public GameObject Player;
+    public float exitForce_X;
+    public float exitForce_Y;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(Player.gameObject.tag == "Player")
+        if (Player.gameObject.tag == "Player")
         {
-            StartCoroutine(Teleport()); 
-        }
+            StartCoroutine(Teleport());
+        } 
     }
 
     IEnumerator Teleport()
     {
-        yield return new WaitForSeconds(1);
-        Player.transform.position = new Vector2(CurrentPortal.transform.position.x, CurrentPortal.transform.position.y);
+        yield return new WaitForSeconds(0.1f);
+        Player.transform.position = new Vector2(CurrentPortal.transform.position.x + exitForce_X, CurrentPortal.transform.position.y + exitForce_Y);
         Debug.Log("Worked");
     }
 
