715acb793ac24f1a6639250f00355225729970a4
CRABBJoshua <115218634+CRABBJoshua@users.noreply.github.com>
2023-03-03 18:01:43 +0000

    Portal Update

--- Assets/Scripts/Custom Scipts/Portal.cs
+++ Assets/Scripts/Custom Scipts/Portal.cs
@@ -9,6 +9,7 @@ public class Portal : MonoBehaviour
     public GameObject Player;
     public float exitForce_X;
     public float exitForce_Y;
+    public bool ShouldTeleport;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
